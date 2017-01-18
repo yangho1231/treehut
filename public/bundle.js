15e0dd02c46454aa7470939796b924faff238801
yangho1231 <yangho1231@gmail.com>
2017-01-17 17:20:41 -0700

    Fixed error

--- public/bundle.js
+++ public/bundle.js
@@ -281,6 +281,67 @@ angular.module('myApp').controller('watchesCtrl', function ($scope, mainSvc) {
 });
 'use strict';
 
+angular.module('myApp').directive('bottomMenu', function () {
+  return {
+    restrict: 'EA',
+    templateUrl: '../../../assets/views/bottompage.html'
+  };
+});
+'use strict';
+
+angular.module('myApp').directive('homeMenu', function () {
+  return {
+    restrict: 'EA',
+    templateUrl: '../../../assets/views/homepage-menu.html',
+
+    link: function link(scope, element, attributes) {
+      $(window).on('scroll', function () {
+
+        if ($(window).scrollTop() > 50) {
+          $('.navbar-brand>img').css({ "filter": "brightness(1) invert(0)" });
+          $('.navbar').css({ "background-color": "white", "border-bottom": "1px solid black" });
+          $('.nav.navbar-nav.navbar-right li').css({ "color": "black" });
+          $('.container-fluid').css({ "height": "15vh" });
+        } else if ($(window).scrollTop() < 50) {
+          $('.navbar-brand>img').css({ "filter": "brightness(0) invert(1)" });
+          $('.navbar').css({ "background-color": "transparent", "border-bottom": "none" });
+          $('#navbar-right li').css({ "color": "white" });
+        }
+      });
+    }
+  };
+});
+
+// $('.infoBox').on('mouseover', function() {
+//
+//   $('.infoBox').animate({height: '400px', width: '700px'});
+// });
+'use strict';
+
+angular.module('myApp').directive('menuLeft', function () {
+  return {
+    restrict: 'EA',
+    templateUrl: '../../../assets/views/menu-left.html'
+  };
+});
+'use strict';
+
+angular.module('myApp').directive('menDirective', function () {
+  return {
+    restrict: 'EA',
+    templateUrl: '../../../assets/views/menu-top.html'
+  };
+});
+'use strict';
+
+angular.module('myApp').directive('menuDirective', function () {
+  return {
+    restrict: 'EA',
+    templateUrl: '../../../assets/views/test.html'
+  };
+});
+'use strict';
+
 angular.module('myApp').service('mainSvc', function ($http, $rootScope) {
   this.customer = {};
   this.cart = [];
@@ -471,65 +532,4 @@ angular.module('myApp').service('mainSvc', function ($http, $rootScope) {
   //   return res;
   // });
 
-});
-'use strict';
-
-angular.module('myApp').directive('bottomMenu', function () {
-  return {
-    restrict: 'EA',
-    templateUrl: '../../../assets/views/bottompage.html'
-  };
-});
-'use strict';
-
-angular.module('myApp').directive('homeMenu', function () {
-  return {
-    restrict: 'EA',
-    templateUrl: '../../../assets/views/homepage-menu.html',
-
-    link: function link(scope, element, attributes) {
-      $(window).on('scroll', function () {
-
-        if ($(window).scrollTop() > 50) {
-          $('.navbar-brand>img').css({ "filter": "brightness(1) invert(0)" });
-          $('.navbar').css({ "background-color": "white", "border-bottom": "1px solid black" });
-          $('.nav.navbar-nav.navbar-right li').css({ "color": "black" });
-          $('.container-fluid').css({ "height": "15vh" });
-        } else if ($(window).scrollTop() < 50) {
-          $('.navbar-brand>img').css({ "filter": "brightness(0) invert(1)" });
-          $('.navbar').css({ "background-color": "transparent", "border-bottom": "none" });
-          $('#navbar-right li').css({ "color": "white" });
-        }
-      });
-    }
-  };
-});
-
-// $('.infoBox').on('mouseover', function() {
-//
-//   $('.infoBox').animate({height: '400px', width: '700px'});
-// });
-'use strict';
-
-angular.module('myApp').directive('menuLeft', function () {
-  return {
-    restrict: 'EA',
-    templateUrl: '../../../assets/views/menu-left.html'
-  };
-});
-'use strict';
-
-angular.module('myApp').directive('menDirective', function () {
-  return {
-    restrict: 'EA',
-    templateUrl: '../../../assets/views/menu-top.html'
-  };
-});
-'use strict';
-
-angular.module('myApp').directive('menuDirective', function () {
-  return {
-    restrict: 'EA',
-    templateUrl: '../../../assets/views/test.html'
-  };
 });
